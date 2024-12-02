cfd1f6ae02461cedf6c493e3f6772abb0a1d8ced
Het Siddhapura <hetsiddhapura200@gmail.com>
2024-12-02 12:50:01 +0530

    Implment initial client side filder structure, database connection with server

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div>
+            <h1>App</h1>
+            <h2>Hello</h2>
+            <h3>Submodule isue fixed</h3>
+        </div>
+    )
+}
+
+export default App;
