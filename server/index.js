3f7ab8633136e682797aa85cd43d77004045e8a3
siddhapurahet <84630752+siddhapurahet@users.noreply.github.com>
2024-12-02 12:51:43 +0530

    Removed connection string in index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://timeCapsuleDBadmin:TimeCapsule@timecapsuledb.yyls4.mongodb.net/'
+const CONNECTION_URL = ''
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
