02a2314557f4e8de2519892aac399b6f5b108dec
Het Siddhapura <het@aarogram.com>
2024-12-03 13:24:13 +0530

    Implement client with posts, form and post component with styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,32 @@
 import React from "react";
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import timeCapsule from './images/timeCapsule.jpg'
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
+import useStyles from './styles';
 
 const App = () => {
+    const classes = useStyles();
+
     return (
-        <div>
-            <h1>App</h1>
-            <h2>Hello</h2>
-            <h3>Submodule isue fixed</h3>
-        </div>
+        <Container maxwidth="lg">
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">TimeCapsule</Typography>
+                <img className={classes.image} src={timeCapsule} alt="TimeCapsule" height="60"/>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     )
 }
 
---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,11 @@
+import React from "react";
+import useStyles from './styles';
+
+const Post = () => {
+    const classes = useStyles();
+    return (
+        <h1>single post</h1>
+    )
+};
+
+export default Post;
