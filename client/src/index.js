3192cfb88ec2c0c974a275c7fc8742901490c8a8
Het Siddhapura <het@aarogram.com>
2024-12-03 18:46:57 +0530

    Implement redux functionality

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {Provider} from 'react-redux';
+import {applyMiddleware, compose} from 'redux';
+import {configureStore} from '@reduxjs/toolkit';
+import {thunk} from 'redux-thunk';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore(reducers, compose(applyMiddleware(thunk)))
+
+ReactDOM.createRoot(
+    <Provider store = {store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root'));
