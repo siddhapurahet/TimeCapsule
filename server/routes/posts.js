bb2af3eb78f23389e8bb74ad6334707de849d4f8
Het Siddhapura <hetsiddhapura200@gmail.com>
2024-12-04 13:52:22 +0530

    Implement route of createPost pushing client side form from frontend to databse
    Implement frontend form to create a timeCapsule

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -4,7 +4,7 @@ import { getPosts, createPost } from '../controllers/posts.js';
 const router = express.Router();
 
 router.get('/', getPosts);
-router.get('/', createPost);
+router.post('/', createPost);
 
 
 export default router;
