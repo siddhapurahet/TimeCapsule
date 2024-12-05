e7c76b99897d513b2f4e4b028fe20107409e21b2
Het Siddhapura <hetsiddhapura200@gmail.com>
2024-12-05 16:08:32 +0530

    Implement structure of post and posts

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = '';
+const CONNECTION_URL = 'mongodb+srv://timeCapsuleDBadmin:TimeCapsule@timecapsuledb.yyls4.mongodb.net/';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
