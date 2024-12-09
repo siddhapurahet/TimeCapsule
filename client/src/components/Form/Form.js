a4cea157dc774655740d037c0654791eb0aaa229
Het Siddhapura <hetsiddhapura200@gmail.com>
2024-12-09 11:09:49 +0530

    Version-1 with Edit, Create, Delete, Like functionality
    Implement constants folder for scalability, added .env

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -7,7 +7,7 @@ import { createPost, updatePost } from "../../actions/posts";
 import { current } from "@reduxjs/toolkit";
 
 const Form = ({currentId, setcurrentId}) => {
-    const [postData, setpostData] = useState({creator: '', title: '', messsage: '', tags: '', selectedFile: ''});
+    const [postData, setpostData] = useState({creator: '', title: '', message: '', tags: '', selectedFile: ''});
     const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null);
     const classes = useStyles();
     const dispatch = useDispatch();
@@ -31,7 +31,7 @@ const Form = ({currentId, setcurrentId}) => {
 
     const clear = () => {
         setcurrentId(null);
-        setpostData({creator: '', title: '', messsage: '', tags: '', selectedFile: ''});
+        setpostData({creator: '', title: '', message: '', tags: '', selectedFile: ''});
     }
     
     return (
@@ -43,7 +43,7 @@ const Form = ({currentId, setcurrentId}) => {
                 <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) => setpostData({...postData, message: e.target.value})} />
                 <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setpostData({...postData, tags: e.target.value.split(',')})} />
                 <div className={classes.fileInput}> <FileBase type="file" multiple={false} onDone={({base64}) => setpostData({...postData, selectedFile: base64})} /></div>
-                <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
+                <Button className={classes.buttonSubmit} style={{ marginBottom: '10px' }} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
                 <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
             </form>
         </Paper>
