1d1744905daf26be58614eb74dcae4f321add9fa
Het Siddhapura <hetsiddhapura200@gmail.com>
2024-12-12 16:25:23 +0530

    Added Cross origin policy

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.setHeader('same-origin-allow-popups');
+  res.setHeader('Cross-Origin-Opener-Policy','same-origin-allow-popups');
   next();
 });
 
