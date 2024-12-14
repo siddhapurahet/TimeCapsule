aa81ef86f47586af53485638afd0bf104083880e
Het Siddhapura <hetsiddhapura200@gmail.com>
2024-12-14 11:43:34 +0530

    Server side Bug fixing of file name extension

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import user from '../models/user';
+import user from '../models/user.js';
 
 export const signin = async(req, res) => {
     const { email, password } = req.body;
